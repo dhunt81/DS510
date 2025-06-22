b2a2a6093feea5b7c9e09d8578cd2ca1f951a7bc
Devin Hunt <devin.hunt@icloud.com>
2025-06-22 10:45:47 -0400

    added example runs to Part A

--- Assignment 4/Assignment 4A - Hunt.py
+++ Assignment 4/Assignment 4A - Hunt.py
@@ -58,3 +58,92 @@ if __name__ == "__main__":
 
     # run the listAboveMean function to print elements above the mean
     listAboveMean(randList, rLMean)
+
+''' Example outputs
+>>> %run "c:\\Users\\devin\\OneDrive\\Documents\\positron\\DS510\\Assignment 4\\Assignment 4A - Hunt.py"
+Enter the size of the list: 
+12
+i= 0 random int = 9
+i= 1 random int = 8
+i= 2 random int = 1
+i= 3 random int = 10
+i= 4 random int = 5
+i= 5 random int = 0
+i= 6 random int = 4
+i= 7 random int = 5
+i= 8 random int = 4
+i= 9 random int = 1
+i= 10 random int = 12
+i= 11 random int = 7
+The random list contains 12 numbers.
+This random list is: [9, 8, 1, 10, 5, 0, 4, 5, 4, 1, 12, 7]
+The list mean is: 5.5
+Here are the elements larger than the mean:
+i= 0 random int= 9
+i= 1 random int= 8
+i= 3 random int= 10
+i= 10 random int= 12
+i= 11 random int= 7
+
+
+>>> %run "c:\\Users\\devin\\OneDrive\\Documents\\positron\\DS510\\Assignment 4\\Assignment 4A - Hunt.py"
+Enter the size of the list: 
+25
+i= 0 random int = 7
+i= 1 random int = 10
+i= 2 random int = 11
+i= 3 random int = 13
+i= 4 random int = 2
+i= 5 random int = 21
+i= 6 random int = 18
+i= 7 random int = 23
+i= 8 random int = 22
+i= 9 random int = 15
+i= 10 random int = 18
+i= 11 random int = 16
+i= 12 random int = 15
+i= 13 random int = 11
+i= 14 random int = 12
+i= 15 random int = 25
+i= 16 random int = 0
+i= 17 random int = 0
+i= 18 random int = 6
+i= 19 random int = 3
+i= 20 random int = 25
+i= 21 random int = 18
+i= 22 random int = 22
+i= 23 random int = 25
+i= 24 random int = 25
+The random list contains 25 numbers.
+This random list is: [7, 10, 11, 13, 2, 21, 18, 23, 22, 15, 18, 16, 15, 11, 12, 25, 0, 0, 6, 3, 25, 18, 22, 25, 25]
+The list mean is: 14.52
+Here are the elements larger than the mean:
+i= 5 random int= 21
+i= 6 random int= 18
+i= 7 random int= 23
+i= 8 random int= 22
+i= 9 random int= 15
+i= 10 random int= 18
+i= 11 random int= 16
+i= 12 random int= 15
+i= 15 random int= 25
+i= 20 random int= 25
+i= 21 random int= 18
+i= 22 random int= 22
+i= 23 random int= 25
+i= 24 random int= 25
+
+
+>>> %run "c:\\Users\\devin\\OneDrive\\Documents\\positron\\DS510\\Assignment 4\\Assignment 4A - Hunt.py"
+Enter the size of the list: 
+3
+i= 0 random int = 1
+i= 1 random int = 1
+i= 2 random int = 2
+The random list contains 3 numbers.
+This random list is: [1, 1, 2]
+The list mean is: 1.3333333333333333
+Here are the elements larger than the mean:
+i= 2 random int= 2
+>>>
+'''
