6c4471cf42916f140352cb626f72b58ea95ef3ca
Devin Hunt <devin.hunt@icloud.com>
2025-07-04 20:01:58 -0400

    added additional testing parameters

--- Assignment 6/Assignment 6B - Hunt.py
+++ Assignment 6/Assignment 6B - Hunt.py
@@ -22,38 +22,18 @@ if __name__ == "__main__":
         #Ask for bid input
         currentBid = int(input("Enter a numeric bid great than 0: "))
 
-        #compare bid to last highest bid
+        # if empty then place the bid
         if highBids.isEmpty():
             highBids.push(currentBid)
             print("new high bid is: {0}".format(currentBid))
-
+            
+        #compare bid to last highest bid
         elif currentBid > highBids.top():
             highBids.push(currentBid)
             print("new high bid is: {0}".format(currentBid))
 
+        #print the stack for testing
+        print("High Bid History is: ", highBids)
+
+        #Ask user if they want to continue
         response = input("Do you wish to continue (y/N): ").upper()
-
-''' Testing
-
-PS C:\Users\devin\OneDrive\Documents\positron\DS510> & C:/Users/devin/AppData/Local/Programs/Python/Python313/python.exe "c:/Users/devin/OneDrive/Documents/positron/DS510/Assignment 6/Assignment 6B - Hunt.py"
-Enter a numeric bid great than 0: 2
-new high bid is: 2
-Do you wish to continue (y/N): y
-Enter a numeric bid great than 0: 6
-new high bid is: 6
-Do you wish to continue (y/N): y
-Enter a numeric bid great than 0: 5
-Do you wish to continue (y/N): y
-Enter a numeric bid great than 0: 16
-new high bid is: 16
-Do you wish to continue (y/N): y
-Enter a numeric bid great than 0: 10
-Do you wish to continue (y/N): y
-Enter a numeric bid great than 0: 16
-Do you wish to continue (y/N): y
-Enter a numeric bid great than 0: 19
-new high bid is: 19
-Do you wish to continue (y/N): n
-PS C:\Users\devin\OneDrive\Documents\positron\DS510> 
-
-'''
